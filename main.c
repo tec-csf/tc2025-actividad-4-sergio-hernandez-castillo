edc1d373cf3301959c6f4419b16b75e4696a748b
Sergio Hernandez Castillo <shcastillo@MacBook-Pro.lan>
2020-09-06 20:13:39 -0500

    Commit inicial.

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+Autór: Sergio Hernandez Castillo
+Matrícula: A01025210
+Descripción: Actividad 4 - Procesos
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Actividad 4");
+
+    return 0;
+}
