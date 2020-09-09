71ac440f7318dc60a374ef0c5995be1a5d583335
Sergio Hernandez Castillo <shcastillo@MacBook-Pro.lan>
2020-09-08 20:26:48 -0500

    Se añadió un switch para la entrada de input

--- main.c
+++ main.c
@@ -5,9 +5,53 @@ Descripción: Actividad 4 - Procesos
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <ctype.h>
 
-int main(){
-    printf("Actividad 4");
+int main(int argc, char * const * argv){
+    int dato;
+    int cantidadDeHijos;
+    char * cvalue = NULL;
+
+    opterr = 0;
+
+    while ((dato = getopt(argc, argv, "n:")) != -1){
+        switch (dato){
+            case 'n':
+                cvalue = optarg;
+
+                if (isdigit(*cvalue) > 0){
+                    cantidadDeHijos = atoi(cvalue);
+                    printf("Cantidad de hijos: %d\n", cantidadDeHijos);
+                }
+
+                else {
+                    printf("No proporcionó un valor numérico\n");
+                }
+            break;
+
+            case '?':
+                if (optopt == 'n'){
+                    fprintf(stderr, "Opción -%c requiere un argumento.\n", optopt);
+                }
+                    
+                else if (isprint (optopt)){
+                    fprintf (stderr, "Opción desconocida '-%c'.\n", optopt);
+                }
+                    
+                else {
+                    fprintf (stderr, "Opción desconocida '\\x%x'.\n", optopt);
+                }
+            
+                return 1;
+
+            default:
+                abort();
+            break;
+        }
+    }
 
     return 0;
 }
