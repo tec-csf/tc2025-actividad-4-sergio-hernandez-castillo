191d2251a1666d103e199d95ad6f5420299f13fe
Sergio Hernandez Castillo <shcastillo@MacBook-Pro.lan>
2020-09-10 00:04:39 -0500

    Se añadió comunicación entre procesos

--- main.c
+++ main.c
@@ -12,15 +12,16 @@ Descripción: Actividad 4 - Procesos
 
 typedef struct {
     int id;
-    double promedio;
+    int promedio;
 } Hijo;
+
+void leer(int * fd);
+void escribir(int * fd);
 
 int main(int argc, char * const * argv){
     int dato;
-    pid_t pid;
     int estado;
     int cantidadDeHijos;
-    int hijosCreados = 0;
     char * cvalue = NULL;
 
     opterr = 0;
@@ -65,6 +66,11 @@ int main(int argc, char * const * argv){
     Hijo * fin = hijos + cantidadDeHijos;
     Hijo * h = hijos;
     int i = 0;
+    int tuberia[2];
+    int hijosCreados = 0;
+    pid_t pid;
+
+    pipe(tuberia);
 
     while ((h < fin) && (i < cantidadDeHijos)){
         pid = fork();
@@ -78,7 +84,8 @@ int main(int argc, char * const * argv){
 
         else if (pid == 0){
             printf("Estamos en el proceso hijo con PID = %d y su padre es PPID = %d\n", getpid(), getppid());
-            exit(0);
+            escribir(tuberia);
+            // exit(0);
         }
 
         else {
@@ -88,6 +95,7 @@ int main(int argc, char * const * argv){
                 if (WIFEXITED(estado)){
                     printf("Ya termino el hijo con PID %d con valor de retorno %d\n", pid, WEXITSTATUS(estado));
                     ++hijosCreados;
+                    leer(tuberia);
                 }
             }
 
@@ -103,4 +111,24 @@ int main(int argc, char * const * argv){
     free(hijos);
 
     return 0;
+}
+
+void leer(int * fd){
+    int c;
+
+    while (1){
+        close(fd[1]);
+        read(fd[0], &c, sizeof(int));
+        printf("--- Recibí un promedio de %d\n", c);
+    }
+}
+
+void escribir(int * fd){
+    int idPadre = getppid();
+    int idHijo = getpid();
+    int promedio = (idPadre + idHijo) / 2;
+
+    close(fd[0]);
+    printf("+++ Envío un promedio de %d\n", promedio);
+    write(fd[1], &promedio, sizeof(int));
 }
